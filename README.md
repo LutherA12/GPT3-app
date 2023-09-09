9407ec87b761a7f1c1e1de01c2684abe4f6739aa
Luther A <106278674+LutherA12@users.noreply.github.com>
2023-09-09 17:14:33 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # React + Vite
 
-As stated, I am using Vite for this React application; This is another project I'm working to understand how to better use Vite, SASS (scss), and organization of file and folder structure as a whole during projects; I'm also working on using objects in my code so that my return statements for each function are not bogged down with strings as well as being DRY; More updates to follow;
-
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -5,12 +5,44 @@ function WhatGPT3() {
   const info = {
     heading: <>The possibilities are beyond your imagination!!</>,
     subtext: <>Explore the library</>,
+    titleOne: <>What is GPT-3</>,
+    textOne: (
+      <>
+        As we've covered a little bit in the intro, GPT-3 is a cutting edge AI
+        that is very similar to Chat GPT with a few key differences! Sounds good
+        right? We think so!
+      </>
+    ),
+    titleTwo: <>Chatbots</>,
+    textTwo: (
+      <>
+        These chatbots will be deployed in various places to support the main
+        "head" of GPT3. Anyone who signs up now will have free access!
+      </>
+    ),
+    titleThree: <>Knowledgebase</>,
+    textThree: (
+      <>
+        GPT3 pulls from an extensive knowledgebase to give you the widest array
+        of answers possible. We are very serious about truth and we want you to
+        have all the information you can at your disposel so that you can make
+        the most informed decision possible!
+      </>
+    ),
+    titleFour: <>Education</>,
+    textFour: (
+      <>
+        Learning and knowledge, that is what we are all about and we ensure that
+        GPT3 is up to speed on the latest and greatest things! Don't believe us?
+        We dare you to try it!
+      </>
+    ),
   };
 
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title={info.titleOne} text={info.textOne} />
       </div>
 
       <div className="gpt3__whatgpt3-heading">
@@ -19,9 +51,9 @@ function WhatGPT3() {
       </div>
 
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title={info.titleTwo} text={info.textTwo} />
+        <Feature title={info.titleThree} text={info.textThree} />
+        <Feature title={info.titleFour} text={info.textFour} />
       </div>
     </div>
   );
